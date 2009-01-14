dfa69ac635520af52444b971e219c42e89b83d22
Samphippen <Samphippen@19eba3ae-e282-11dd-9059-71fac19e68c6>
2009-01-14 21:55:00 +0000

    improved phippers simple self test

--- dh/seedtest.py
+++ dh/seedtest.py
@@ -3,4 +3,5 @@ from hashlib import sha256
 from random import randint
 
 for i in range(0,256):
+	
 	print sha256(sha256(str(time())).hexdigest() + sha256(str(randint(0,2**256))).hexdigest()).hexdigest()
