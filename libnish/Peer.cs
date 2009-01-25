ecbbf4746fb7a7033cae0fb234db62c4cba51b21
unik64 <unik64@19eba3ae-e282-11dd-9059-71fac19e68c6>
2009-01-25 19:57:24 +0000

    Nish nish nish nish! Initial commit (on my part).  It don't do nothing and it ain't even all architecturateded yet.

---
+++ libnish/Peer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+using System.Text;
+
+namespace libnish
+{
+    public class Peer
+    {
+        public float Karma = 1f;
+
+
+    }
+}
---
+++ libnish/PeerFinders/PeerFinder.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+using System.Text;
+
+namespace libnish.PeerFinders
+{
+    public struct PotentialPeer
+    {
+        public string IP;
+        public int Port;
+
+        public bool TryConnect(out Peer Peer)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public abstract class PeerFinder
+    {
+        private List<Peer> PotentialPeers;
+
+        
+    }
+}
