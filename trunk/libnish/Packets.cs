76704deae63af7ab76d098ccaca7a62eb141280a
unik64 <unik64@19eba3ae-e282-11dd-9059-71fac19e68c6>
2009-01-25 19:57:24 +0000

    Nish nish nish nish! Initial commit (on my part).  It don't do nothing and it ain't even all architecturateded yet.

---
+++ trunk/libnish/Packets.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+using System.Text;
+
+namespace libnish
+{
+    public class Packet
+    {
+        byte[] Content;
+
+        public byte[] ToUnencryptedByteArray()
+        {
+            return Content;
+        }
+    }
+}
---
+++ trunk/libnish/PeerManager.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+using System.Text;
+
+namespace libnish
+{
+    public static class PeerManager
+    {
+    }
+}
