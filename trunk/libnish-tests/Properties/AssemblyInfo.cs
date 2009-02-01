d3edab39da40cb962f3b82c0d7965fbab38576a8
unik64 <unik64@19eba3ae-e282-11dd-9059-71fac19e68c6>
2009-02-01 22:42:23 +0000

    Updated libnish a little, added libnish-tests which currently contains a grand total of... 1 test.  Needs NUnit to be installed on the machine, which is why I put it as a separate project that can be excluded from the normal compile if you don't have NUnit.

---
+++ trunk/libnish-tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("libnish-tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("libnish-tests")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2009")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("25c0ca99-a690-4e27-bd0e-e5044961443e")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
--- trunk/libnish/PeerManager.cs
+++ trunk/libnish/PeerManager.cs
@@ -2,10 +2,24 @@
 using System.Collections.Generic;
 
 using System.Text;
+using libnish.PeerFinders;
+using System.Threading;
 
 namespace libnish
 {
     public static class PeerManager
     {
+        static List<Peer> Peers = new List<Peer>();
+        static List<PeerFinder> PeerFinders = new List<PeerFinder>();
+
+        static Thread P2PThread;
+
+        public static void Initialise(Peer[] InitialPeers, PeerFinder[] InitialPeerFinders)
+        {
+            Peers.AddRange(InitialPeers);
+            PeerFinders.AddRange(InitialPeerFinders);
+            
+            //P2PThread = new Thread(new ThreadStart(
+        }
     }
 }
