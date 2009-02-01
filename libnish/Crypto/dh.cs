c8a1f5f7989be3c09037374231ff489d8bacf173
Samphippen <Samphippen@19eba3ae-e282-11dd-9059-71fac19e68c6>
2009-02-01 19:47:09 +0000

    added dh.cs

---
+++ libnish/Crypto/dh.cs
@@ -0,0 +1,20 @@
+// dh.cs created with MonoDevelop
+// User: sam at 18:52 26/01/2009
+//
+// To change standard headers go to Edit->Preferences->Coding->Standard Headers
+//
+
+using System;
+
+namespace libnish
+{
+	
+	
+	public class dh
+	{
+		
+		public dh()
+		{
+		}
+	}
+}
