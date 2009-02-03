d60a92f0274572c731527638db9e89eaa1a1c16a
Samphippen <Samphippen@19eba3ae-e282-11dd-9059-71fac19e68c6>
2009-02-03 19:25:05 +0000

    added gmp rng to math; started dh using gmp

--- trunk/libnish/Crypto/dh.cs
+++ trunk/libnish/Crypto/dh.cs
@@ -7,23 +7,36 @@
 using System;
 using GMP;
 
-namespace libnish
+
+namespace libnish.Crypto
 {
 	
 	
 	public class dh
 	{
 		
-		private Random Generator = new Random();
+		//private Random Generator = new Random();
 		public GMP.Integer g,p,a,k1,k2,key;
+		
 		public dh()
 		{
-			g = 0;
-			p = 0;
-			a = 0;
-			k1 = 0;
-			k2 = 0;
-			key = 0;
+			g = new GMP.Integer(0);
+			p = new GMP.Integer(0);
+			a = new GMP.Integer(0);
+			k1 = new GMP.Integer(0);
+			k2 = new GMP.Integer(0);
+			key = new GMP.Integer(0);
+		}
+		//G and P are the variables that everyone knows.
+		public void generateGP(){
+			g = Math.math.randgmp(256);
+			p = Math.math.randgmp(256);
+		}
+		
+		//A is the private exponent of person a, (g^a) mod p is calculated
+		public void generateA(){
+			GMP.Integer t = new GMP.Integer(0);
+			
 		}
 		
 	}
