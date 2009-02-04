4234202e81671f923b2c420334ce8e0701e54538
Samphippen <Samphippen@19eba3ae-e282-11dd-9059-71fac19e68c6>
2009-02-04 20:12:20 +0000

    fixed aes, used proper chaining mode

--- trunk/libnish/Crypto/aes.cs
+++ trunk/libnish/Crypto/aes.cs
@@ -16,42 +16,33 @@ namespace libnish
 	public class aes
 	{
 		private Rijndael handler;
-		private byte[] key;
-		private byte[] IV;
+		
 
         public aes()
         {
             handler = RijndaelManaged.Create();
             handler.GenerateIV();
-            IV = handler.IV;
-
             // When no key is provided, we generate one.
-            handler.GenerateKey();
-            key = handler.Key;
+            handler.GenerateKey();   
+			handler.Mode = CipherMode.CBC;
         }
 
 		public aes(byte[] pkey)
 		{
             handler = RijndaelManaged.Create();
             handler.GenerateIV();
-            IV = handler.IV;
-
-			handler.Key = pkey;
-			key = pkey;
+            handler.Key = pkey;
+			handler.Mode = CipherMode.CBC;
+			
+			
 		}
 
 		public byte[] decrypt(byte[] ciphertext)
         {
             // Use the provided byte[] buffer as a backing store to read out of
 			MemoryStream t = new MemoryStream(ciphertext);
-
-            // Setup
-			handler.Key = key;
-			handler.IV = IV;
-
             // Stream automatically decrypts as we read data out of it.
 			CryptoStream Decryptor = new CryptoStream(t, handler.CreateDecryptor(), CryptoStreamMode.Read);
-
             // Read data back out of the CryptoStream.
             byte[] plaintext = new byte[Decryptor.Length];
             Decryptor.Read(plaintext, 0, (int) Decryptor.Length); // if it doesn't fit in an int, its > 2gb anyway. deserves to crash...
@@ -62,12 +53,7 @@ namespace libnish
 		public byte[] encrypt(byte[] plaintext)
         {
 			MemoryStream t = new MemoryStream(plaintext);
-
-			handler.Key = key;
-			handler.IV = IV;
-
 			CryptoStream Encryptor = new CryptoStream(t, handler.CreateEncryptor(), CryptoStreamMode.Write);
-
             byte[] ciphertext = new byte[Encryptor.Length];
             Encryptor.Read(ciphertext, 0, (int) Encryptor.Length);
 
