b200a2d4056875a3b38b89d0d69852b54b590154
Samphippen <Samphippen@19eba3ae-e282-11dd-9059-71fac19e68c6>
2009-02-14 22:50:09 +0000

    added rsa stuff

---
+++ libnish/Crypto/rsa.cs
@@ -0,0 +1,44 @@
+// rsa.cs created with MonoDevelop
+// User: sam at 21:25 12/02/2009
+//
+// To change standard headers go to Edit->Preferences->Coding->Standard Headers
+//
+
+using System;
+using Mono.Math;
+
+
+namespace libnish.Crypto
+{
+	
+	
+	public class rsa
+	{
+		private BigInteger p,q,n,e,d;
+		public rsa()
+		{
+			p = Math.math.makePrime(8192);
+			q = Math.math.makePrime(8192);
+			n = p*q;
+			BigInteger store = (p-1)*(q-1);
+			if (store.GCD(65537) == 1){
+				e = 65537;
+			} else{
+				int i = 65539;
+				while (store.GCD(i) != 1){
+					i += 2;
+				}
+				e = i;
+			}
+			// this next bit is fucking complicated
+			// this is also the slowest possible method
+			// for doing this
+			// de = 1 mod store
+			// d.e = - q.store = 1
+			// solve using euler.
+			d = Math.math.modinvsolve(e,store);
+			
+			
+		}
+	}
+}
