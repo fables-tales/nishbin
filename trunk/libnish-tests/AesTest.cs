d8f4c514034701b27341a3c4351f57554d310558
Samphippen <Samphippen@19eba3ae-e282-11dd-9059-71fac19e68c6>
2009-02-21 16:01:35 +0000

    added a series of tests

---
+++ trunk/libnish-tests/AesTest.cs
@@ -0,0 +1,45 @@
+// AesTest.cs created with MonoDevelop
+// User: sam at 20:40 04/02/2009
+//
+// To change standard headers go to Edit->Preferences->Coding->Standard Headers
+//
+using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+using libnish.Crypto;
+
+namespace libnish_tests
+{
+	
+	[TestFixture]
+	public class AesTest
+	{
+		[Test]
+		[Description("Test encryption")]
+		public void EncTest(){
+			aes aesobj = new aes();
+			byte[] dave = new byte[32];
+			for (int i = 0;i<32;i++){
+				dave[i] = 0;
+			}
+			dave[0] = 32;
+			dave[1] = 127;
+			byte[] dave2 = new byte[32];
+			for (int i = 0;i<32;i++){
+				dave2[i] = 0;
+			}
+			dave2[0] = 12;
+			dave2[1] = 47;
+			
+			byte[] enc = aesobj.encrypt(dave);
+			byte[] enc2 = aesobj.encrypt(dave2);
+			Console.WriteLine(dave.Length);
+			Console.WriteLine(enc.Length);
+			byte[] dec = aesobj.decrypt(enc);
+			Console.WriteLine(dec);
+			Assert.AreEqual(dave,dec);
+			
+		}
+	}
+}
---
+++ trunk/libnish-tests/RSATest.cs
@@ -0,0 +1,33 @@
+// RSATest.cs created with MonoDevelop
+// User: sam at 14:10 21/02/2009
+//
+// To change standard headers go to Edit->Preferences->Coding->Standard Headers
+//
+
+using System;
+
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+using libnish.Crypto;
+
+
+namespace libnish_tests
+{
+	
+	[TestFixture]
+	public class RSATest
+	{
+		
+		[Test]
+        [Description("Simple tests for Rsa")]
+		public void SimpleRSA(){
+			DateTime f = DateTime.Now;
+			RSAKeyPair k = new RSAKeyPair();
+			DateTime p = DateTime.Now;
+			System.Console.WriteLine((p-f).TotalSeconds);
+			Assert.That((p - f).Minutes <= 2);
+			
+		}
+	}
+}
