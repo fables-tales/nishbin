0860b5b696cd9393c97ff2bb63c8996d144a37c1
Samphippen <Samphippen@19eba3ae-e282-11dd-9059-71fac19e68c6>
2009-02-21 16:01:35 +0000

    added a series of tests

---
+++ libnish-tests/UUIDTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+using libnish;
+
+namespace libnish_tests
+{
+    [TestFixture]
+    public class UUIDTest
+    {
+        [Test]
+        [Description("test for uuid")]
+        public void simpleUUIDTest()
+        {
+			string s  = libnish.Crypto.UUID.getUUID();
+			Assert.AreEqual(libnish.Crypto.UUID.verifyuuid(s),true);
+        }
+
+       
+
+    }
+}
