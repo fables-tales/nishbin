c1cb75908b3ef563c76db283c0b1f22689f5cd6f
unik64 <unik64@19eba3ae-e282-11dd-9059-71fac19e68c6>
2009-03-07 14:54:14 +0000

    i forgot

---
+++ libnish/EncryptedConnection.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.IO;
+using System.Security.Cryptography;
+using libnish.Crypto;
+using Mono.Math;
+
+namespace libnish
+{
+    public abstract class EncryptedConnection
+    {
+        string IP;
+        int Port;
+
+        TcpClient TcpClient;
+
+        Limits Limits;
+
+        bool OurEndNeedsToDoTheHandshake = false;
+        bool HandsShaken = false;
+
+        BinaryReader br;
+        BinaryWriter bw;
+
+        BigInteger key;
+
+        private EncryptedConnection(TcpClient LiveConnection, string IPAddress, int Port, Limits Limits, bool Outward)
+        {
+            TcpClient = LiveConnection;
+            IP = IPAddress;
+            this.Port = Port;
+            this.Limits = Limits;
+            OurEndNeedsToDoTheHandshake = Outward;
+
+            br = new BinaryReader(TcpClient.GetStream());
+            bw = new BinaryWriter(TcpClient.GetStream());
+        }
+
+
+
+        private void AwaitResponse(byte NumBytesResponseRequired, int TimeoutMs)
+        {
+            DateTime startTime = DateTime.UtcNow;
+
+            while (TcpClient.Available < NumBytesResponseRequired)
+            {
+                Thread.Sleep(10); // checking 100x a sec ought to be more than fast enough
+                // if you're some bizarro person from the future who needs to do this faster, you can change this bloody const yourself
+
+                if ((DateTime.UtcNow - startTime).TotalMilliseconds >= TimeoutMs)
+                    throw new TimeoutException("SendAndAwaitResponse: Peer failed to respond in the given timeout period. Sorry 'bout that...");
+            }
+        }
+
+        private void EncryptAndSend(byte[] data)
+        {
+
+        }
+
+        private void ReceiveAndDecrypt(byte[] data)
+        {
+
+        }
+
+        private void Handshake()
+        {
+            // If already started/done handshake because remote peer initiated handshake, DO NOT error here!!
+            // Need to just fail silently.
+
+            // Now, what to do if both handshake at the same time is something completely different... :<
+            if (HandsShaken)
+                return;
+            else
+                HandsShaken = true;
+
+            /*
+	         * The actual dh protocol:
+	         * person a: generate g and p, send to person b
+	         * person b: accept g and p and respond with an acknowledgement (usually a hash of a + b)
+	         * person a: generate a, compute k1 = (g^a) mod p
+	         * person b: generate b, compute k2 = (g^a) mod p
+	         * person a: send k1 to person b
+	         * person b: send k2 to person a
+	         * person a: compute key = (k2^b) mod p
+	         * person b: compute key = (k1^b) mod p */
+
+            dh dh = new dh();
+
+            byte[] hash;
+
+            switch (OurEndNeedsToDoTheHandshake)
+            {
+                case true: // Person A!
+                    // person a: generate g and p, send to person b
+                    dh.generateGP();
+                    bw.Write(dh.g.GetBytes());
+                    bw.Write(dh.p.GetBytes());
+                    
+                    // Compute the hash ourselves, to compare with the data we sure are gonna receive right about now
+                    hash = ComputeSHA256Hash(dh.g.GetBytes(), dh.p.GetBytes());
+                    byte[] bsHash = br.ReadBytes(32);
+
+                    for (int i = 0; i < 32; i++)
+                        if (bsHash[i] != hash[i])
+                            throw new InvalidDataException("Person B failed to compute the hash. Computer error. Virus = very yes");
+
+                    // person a: generate a, take person b's a and store as b, compute k1 = (g^a) mod p
+                    dh.generateA();
+                    // (send it so the person B can take our a)
+                    bw.Write(dh.a.GetBytes());
+                    // take their a and call it b.
+                    dh.b = new BigInteger(br.ReadBytes(32));
+                    dh.computeK1();
+
+
+                    // person a: send k1 to person b
+                    bw.Write(MakeIt32Bytes(dh.k1.GetBytes()));
+                    // (get their k1, or 'k2' as we like to call it.)
+                    dh.k2 = new BigInteger(br.ReadBytes(32));
+
+                    // compute.
+                    dh.computeKey();
+
+                    key = dh.key;
+
+                    break;
+
+                case false: // Person B!
+                    // person b: accept g and p and respond with an acknowledgement (usually a hash of a + b)
+                    dh.g = new BigInteger(br.ReadBytes(32));
+                    dh.p = new BigInteger(br.ReadBytes(32));
+                    hash = ComputeSHA256Hash(dh.g.GetBytes(), dh.p.GetBytes());
+                    bw.Write(hash);
+
+                    // person b: generate a, take person a's a and store as b, compute k2 (our k1) = (g^a) mod p
+                    dh.generateA();
+                    // (send it so that A can take our a and call it b)
+                    bw.Write(dh.a.GetBytes());
+                    dh.b = new BigInteger(br.ReadBytes(32));
+                    dh.computeK1();
+
+                    // person b: send k2 (our k1) to person a
+                    bw.Write(MakeIt32Bytes(dh.k1.GetBytes()));
+                    dh.k2 = new BigInteger(br.ReadBytes(32));
+
+                    // compute.
+                    dh.computeKey();
+
+                    key = dh.key;
+                    
+                    break;
+            }
+        }
+
+        private byte[] MakeIt32Bytes(byte[] LessThan32Bytes)
+        {
+            List<byte> output = new List<byte>();
+
+            for (int i = 32 - LessThan32Bytes.Length; i > 0; i++)
+                output.Add(0);
+
+            output.AddRange(LessThan32Bytes);
+
+            return output.ToArray();
+        }
+
+        private byte[] ComputeSHA256Hash(byte[] firstHalfOfInput, byte[] secondHalfOfInput)
+        {
+            List<byte> bl = new List<byte>();
+            bl.AddRange(firstHalfOfInput);
+            bl.AddRange(secondHalfOfInput);
+
+            return ComputeSHA256Hash(bl.ToArray());
+        }
+
+        private byte[] ComputeSHA256Hash(byte[] input)
+        {
+            SHA256 sha = new SHA256Managed();
+            byte[] hash = sha.ComputeHash(input);
+            return hash;
+        }
+    }
+}
