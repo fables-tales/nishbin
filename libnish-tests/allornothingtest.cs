fd7634bd9a2d108329d38d07be6566ec8616db7b
Samphippen <Samphippen@19eba3ae-e282-11dd-9059-71fac19e68c6>
2009-03-07 15:35:22 +0000

    to make a barrel roll

---
+++ libnish-tests/allornothingtest.cs
@@ -0,0 +1,31 @@
+// RSATest.cs created with MonoDevelop
+// User: sam at 14:10 21/02/2009
+//
+// To change standard headers go to Edit->Preferences->Coding->Standard Headers
+//
+
+using System;
+
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+using libnish.Crypto;
+
+
+namespace libnish_tests
+{
+	
+	[TestFixture]
+	public class allornothingtest
+	{
+		
+		[Test]
+        [Description("bees")]
+		public void allornothing_a_barrel_roll(){
+			libnish.Crypto.allornothingtransform sample = new allornothingtransform(new byte[] {13,12,34,12});
+			Assert.AreEqual(sample.message.Length,1024);
+			
+			
+		}
+	}
+}
