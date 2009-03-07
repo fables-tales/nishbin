0de4b2e10698d087d4e053ce100b9c2fa9a6e3bd
Samphippen <Samphippen@19eba3ae-e282-11dd-9059-71fac19e68c6>
2009-03-07 15:57:51 +0000

    commit it and i'll put it through a real compiler

--- libnish/Crypto/allornothingtransform.cs
+++ libnish/Crypto/allornothingtransform.cs
@@ -5,7 +5,7 @@
 //
 
 using System;
-
+using System.Collections.Generic;
 
 namespace libnish.Crypto
 {
@@ -13,76 +13,34 @@ namespace libnish.Crypto
 	
 	public class allornothingtransform
 	{
-		public byte[] message;
-		public byte[,] chunks;
+		public System.Collections.Generic.List<byte[]> message = new List<byte[]>();
+		public System.Collections.Generic.List<byte[]> chunks = new List<byte[]>();
 		public byte[] key;
 		public byte[] iv;
-		private byte[] hashkey;
 		
-		public allornothingtransform(byte[] message){
-			if (message.Length % 1024 != 0){
-				byte[] newmessage = new byte[((message.Length/1024)+1)*1024];
-				message.CopyTo(newmessage,0);
-				for (int i=message.Length;i<newmessage.Length;i++){
-					newmessage[i] = 0;
-				
+		public allornothingtransform(byte[] file){
+			this.message.Clear();
+			int i = 0;
+			for (i=0;i*1024<file.Length-1024;i++){
+				byte[] copy = new byte[1024]();
+				Array.Copy(file,i*1024,copy,0,1024);
+				message.Add(copy);
+			}
+			if (file.Length % 1024 != 0){
+				byte[] copy = new byte[1024]();
+				i=0;
+				for(i=0;i<file.Length;i++){
+					copy[i] = file[i]; 
 				}
-				this.message = newmessage;
-			} else {
-				this.message = message;
-			}
-			int index = 0;
-			int counter = 0;
-			
-			chunks = new byte[1024,this.message.Length/1024];
-			for (index=0;index<1024;index++){
-				for (counter=0;counter<this.message.Length/1024;index++){
-					chunks[index,counter] = this.message[index*1024+counter];
+				while (i < 1024){
+					copy[i] = 0;
+					i++;
 				}
+				message.Add(copy);
 			}
-			this.generateKeyForThis();
+			
 			
 		}
-		public allornothingtransform(byte[,] chunks, byte[] key,byte[] iv){
-			if (chunks.GetLength(1) == 1024 && chunks.GetLength(0) > 0){
-				this.chunks = chunks;
-				this.iv = iv;
-				this.key = key;
-			}
-		}
-		public void generateKeyForThis(){
-			this.key = new byte[this.chunks.GetLength(0)*256];
-			this.iv = new byte[this.chunks.GetLength(0)*256];
-			for(int i = 0;i<this.key.Length;i++){
-				key[i] = Math.math.getRandom(8).GetBytes()[0];
-				iv[i] = Math.math.getRandom(8).GetBytes()[0];
-			}
-		}
-		public void assembleKey(){
-			System.Security.Cryptography.SHA256 hasher = new System.Security.Cryptography.SHA256Managed();
-			this.hashkey = hasher.ComputeHash(this.key);
-		}
-		
-		public byte[,] decrypt(){
-			byte[,] buffer = new byte[this.chunks.GetLength(0),this.chunks.GetLength(1)];
-			aes aeshandler = new aes(this.key,this.iv);
-			
-			for(int i=0;i<buffer.GetLength(0);i++){
-					buffer[i] = aeshandler.decrypt(this.chunks[i]);
-			}
-			return buffer;
-		}
-		public byte[,] encrypt(){
-			
-			byte[,] buffer = new byte[this.chunks.GetLength(0),this.chunks.GetLength(1)];
-			aes aeshandler = new aes(this.key,this.iv);
-			
-			for(int i=0;i<buffer.GetLength(0);i++){
-					buffer[i] = aeshandler.encrypt(this.chunks[i]);
-			}
-			return buffer;
-		}
-		
 		
 	}
 }
