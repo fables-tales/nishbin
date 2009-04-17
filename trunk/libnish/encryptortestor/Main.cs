34f218c081d62f7028ae79b06deb6da0205ba6fb
Samphippen <Samphippen@19eba3ae-e282-11dd-9059-71fac19e68c6>
2009-04-17 16:40:10 +0000

    aes thing

--- trunk/libnish/encryptortestor/Main.cs
+++ trunk/libnish/encryptortestor/Main.cs
@@ -5,6 +5,7 @@
 //
 using System;
 using libnish.Crypto;
+using libnish;
 
 namespace encryptortestor
 {
@@ -12,67 +13,27 @@ namespace encryptortestor
     {
         public static void Main(string[] args)
         {
-            byte[] blocks = libnish.Crypto.Math.math.getRandom(256).GetBytes();
-            byte[] moreblocks = libnish.Crypto.Math.math.getRandom(128).GetBytes();
             aes aes = new aes();
-            byte[] enc1 = aes.encrypt(blocks);
-            byte[] enc2 = aes.encrypt(moreblocks);
-            byte[] dec1 = aes.decrypt(enc1);
-            byte[] dec2 = aes.decrypt(enc2);
-            bool equal = true;
-            for(int i=0;i<blocks.Length;i++){
-                if (blocks[i] != dec1[i]){
-                    equal = false;
-                    break;
+            byte[] someblocks = libnish.Crypto.Math.math.getRandom(128*34).GetBytes();
+            byte[] somemoreblocks = libnish.Crypto.Math.math.getRandom(128*12).GetBytes();
+            byte[] enc1 = aes.Encrypt(someblocks);
+            byte[] enc2 = aes.Encrypt(somemoreblocks);
+            byte[] dec1 = aes.Decrypt(enc1);
+            byte[] dec2 = aes.Decrypt(enc2);
+            Console.Out.WriteLine(dec1.Length == someblocks.Length);
+            Console.Out.WriteLine(dec2.Length == somemoreblocks.Length);
+            Console.ReadLine();
+            for (int i =0;i<someblocks.Length;i++){
+                if (someblocks[i] != dec1[i]){
+                    Console.Out.WriteLine("failmonkey");
                 }
-            }
-            if (equal){
-                Console.Out.WriteLine("first dataset decrypted");
                 
-            } else {
-                Console.Out.WriteLine("first dataset didn't decrypt");
             }
-            equal = true;
-            for (int i =0;i<moreblocks.Length;i++){
-                if (moreblocks[i] == dec2[i]){
-                    equal = false;
-                    break;
+            for (int i=0;i<somemoreblocks.Length;i++){
+                if (somemoreblocks[i] != dec2[i]){
+                    Console.Out.WriteLine("failmonkey2");
                 }
             }
-            if (equal){
-                Console.Out.WriteLine("second dataset decrypted");
-            } else {
-                Console.Out.WriteLine("second dataset didn't decrypt");
-            }
-            Console.Out.WriteLine("now testing non-simultaneous decryption");
-            aes aes2 = new aes();                        
-            byte[] blocks2 = libnish.Crypto.Math.math.getRandom(256*3).GetBytes();
-            byte[] enc = aes2.encrypt(blocks2);
-            byte[] encblock1 = new byte[16];
-            byte[] decblock1 = new byte[16];
-            Array.Copy(enc,0,encblock1,0,16);
-            decblock1 = aes2.decrypt(encblock1);
-            equal = true;
-            for (int i=0;i<16;i++){
-                if (decblock1[i] != blocks2[i]){
-                    equal = false;
-                    Console.Out.WriteLine("failbacon");
-                    break;
-                    
-                }
-            }    
-            Array.Copy(enc,16,encblock1,0,16);
-            decblock1 = aes2.decrypt(encblock1);
-            equal = true;
-            for (int i=0;i<16;i++){
-                if (decblock1[i] != blocks2[i+16]){
-                    equal = false;
-                    Console.Out.WriteLine("failbacon2");
-                    break;
-                    
-                }
-            }
-            
         }
     }
 }
